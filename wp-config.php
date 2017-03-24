2308164af08a8583c35a2a04974cff934d3aa295
nguyenthuan155632 <nt.apple.it@gmail.com>
2017-03-24 23:28:46 +0700

    change db config point to RDS

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define('DB_NAME', 'vensera');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'vensera');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '15563200025ff0');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'vensera.c0mbfmyufmzf.us-west-2.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
