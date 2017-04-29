6c63dd65de50b0323317d72aedb6f7cc3c9529a5
root <root@ip-172-31-24-171.ap-northeast-1.compute.internal>
2017-04-29 15:15:34 +0000

    backup before changing server

---
+++ wp-content/cache/wpfc-minified/b1b7edf4c0ba810824e7ca3dfc36262d/1492411253index.js
@@ -0,0 +1,12 @@
+// source --> https://www.vensera.com/wp-content/themes/sw-maxshop/js/slidegallery.js 
+(function($) {
+			jQuery(document).ready(function($) {
+				//console.log(yaSlideshow);
+				$.each(yaSlideshow, function(key, value) {
+					$('#yaSlideshow-'+key).carousel({
+						interval: value['interval']
+					});
+				});
+			});
+})(jQuery);
+			//-->;
---
+++ wp-content/plugins/facebook-members/includes/setting-page/crunchify-right-panel1.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Crunchify.com
+* Plugin: Facebook Members
+*/
+?>
+<br>
+<div id="side-info-column" class="inner-sidebar">
+	<div class="postbox">
+		<h3 class="hndle">
+			<span>Facebook Page Likebox</span>
+		</h3>
+
+		<div class="inside">
+			<ul>
+				<div align="left">
+				<br>
+					<a href="http://crunchify.com/facebook-members/">Home Page</a> link.
+				<br> 
+				</div>
+			</ul>
+		</div>
+	</div>
+</div>
